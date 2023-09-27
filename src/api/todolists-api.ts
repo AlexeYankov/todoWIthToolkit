453231e69120501987301fbc8e84da332c063557
Alexey Yankov <yankovav.wm@gmail.com>
2023-09-27 18:15:22 +0300

    fixed api-key

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '271ea311-898b-4149-afce-d27b78b2324e'
     }
 }
 const instance = axios.create({
